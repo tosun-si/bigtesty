3af67d1ebd9625a4f6bb97c91e9dff9b003654a1
Mazlum TOSUN <mazlum.tosun@gmail.com>
2024-05-03 18:36:54 +0200

    Add the GCP project ID, GCP region and the IaC backend URL as variables in the CLI. Update the CI part in the examples folder after added the project ID, region and backend URL as parameters in the CLI.

--- bigtesty/cli/main.py
+++ bigtesty/cli/main.py
@@ -5,7 +5,7 @@ from typing_extensions import Annotated
 
 from bigtesty.cli.cli_env_vars import ROOT_TEST_FOLDER_KEY, ROOT_TABLES_FOLDER_KEY, TABLES_CONFIG_FILE_KEY, \
     KEEP_INFRA_KEY, BIGTESTY_STACK_NAME_KEY, PULUMI_CONFIG_PASSPHRASE_KEY, PULUMI_BACKEND_URL_KEY, IAC_BACKEND_URL_KEY, \
-    BIGTESTY_STACK_NAME_VALUE, PULUMI_CONFIG_PASSPHRASE_VALUE
+    BIGTESTY_STACK_NAME_VALUE, PULUMI_CONFIG_PASSPHRASE_VALUE, GOOGLE_PROJECT_KEY, GOOGLE_REGION_KEY
 from bigtesty.infra.launch_tests_ephemeral_infra import launch_tests_ephemeral_infra
 
 app = typer.Typer()
@@ -13,6 +13,30 @@ app = typer.Typer()
 
 @app.command("test", help="Run tests")
 def run_tests(
+        project: Annotated[
+            str,
+            typer.Option(
+                "--project",
+                envvar=GOOGLE_PROJECT_KEY,
+                help="GCP project ID."
+            )
+        ],
+        region: Annotated[
+            str,
+            typer.Option(
+                "--region",
+                envvar=GOOGLE_REGION_KEY,
+                help="GCP region."
+            )
+        ],
+        iac_backend_url: Annotated[
+            str,
+            typer.Option(
+                "--iac-backend-url",
+                envvar=IAC_BACKEND_URL_KEY,
+                help="IaC backend URL for the ephemeral infra used for the tests."
+            )
+        ],
         root_test_folder: Annotated[
             str,
             typer.Option(
@@ -47,14 +71,19 @@ def run_tests(
         ] = False):
     print(f"####################### The CLI is invoked with params : ")
 
+    print(f"project : {project}")
+    print(f"region : {region}")
+    print(f"iac-backend-url : {iac_backend_url}")
     print(f"root-test-folder : {root_test_folder}")
     print(f"root-tables-folder : {root_tables_folder}")
     print(f"tables-config-file : {tables_config_file_path}")
     print(f"keep-infra : {keep_infra}")
 
-    os.environ[BIGTESTY_STACK_NAME_KEY] = BIGTESTY_STACK_NAME_VALUE
+    os.environ[GOOGLE_PROJECT_KEY] = project
+    os.environ[GOOGLE_REGION_KEY] = region
+    os.environ[PULUMI_BACKEND_URL_KEY] = iac_backend_url
     os.environ[PULUMI_CONFIG_PASSPHRASE_KEY] = PULUMI_CONFIG_PASSPHRASE_VALUE
-    os.environ[PULUMI_BACKEND_URL_KEY] = os.environ[IAC_BACKEND_URL_KEY]
+    os.environ[BIGTESTY_STACK_NAME_KEY] = BIGTESTY_STACK_NAME_VALUE
 
     launch_tests_ephemeral_infra(
         root_test_folder=root_test_folder,
