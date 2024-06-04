73fb4461af10a00b0d4ca781026cdf0c647c63d3
Mazlum TOSUN <mazlum.tosun@gmail.com>
2024-06-04 18:40:14 +0200

    Upgrade the version of BigTesty to 0.1.0a9 to solve the release issue.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.path.isfile(path):
 
 setup(
     name="bigtesty",
-    version="0.1.0a8",
+    version="0.1.0a9",
     entry_points='''
         [console_scripts]
         bigtesty=bigtesty.cli.main:run
