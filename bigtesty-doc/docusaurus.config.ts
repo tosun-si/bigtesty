38baf5a2b29f8ac1073c7e699fe4780dd7868cc3
Mazlum TOSUN <mazlum.tosun@gmail.com>
2024-07-13 01:59:32 +0200

    Add the BigTesty Social Card to the documentation.

--- bigtesty-doc/docusaurus.config.ts
+++ bigtesty-doc/docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    // image: 'img/docusaurus-social-card.jpg',
+    image: 'img/bigtesty_social_card.jpg',
     navbar: {
       title: 'BigTesty',
       logo: {
